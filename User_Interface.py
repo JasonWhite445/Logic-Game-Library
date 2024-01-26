ba675b205f27591ff9f7a906f31e72e80a1c1b6d
noahsmith774 <noah.smith774@my.simpson.edu>
2024-01-25 22:50:35 -0600

    initializes window, sets colors and widths, and draws game board for User_Interface.py

--- User_Interface.py
+++ User_Interface.py
@@ -1,5 +1,32 @@
 import pygame
 
+WIDTH = 600
+background_color = (255, 255, 255)
 
-print("Work in Progress :/")
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((WIDTH, WIDTH))
+    pygame.display.set_caption("Sudoku Solver")
+    window.fill(background_color)
+
+    # https://www.pygame.org/docs/ref/draw.html
+    for i in range(0, 10):
+        # drawing thicker lines for sub-grids
+        if i % 3 == 0:
+            pygame.draw.line(window, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 4)
+            pygame.draw.line(window, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 4)
+        pygame.draw.line(window, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
+        pygame.draw.line(window, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
+    pygame.display.update()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+
+main()
+
 
