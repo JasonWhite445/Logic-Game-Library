68e43f7026b5f17e4fb1cd4fb219e74ef62146b2
AdamGruhn <akgoggles@yahoo.com>
2024-03-19 11:50:06 -0500

    Removed unused functions

--- Sudoku.py
+++ Sudoku.py
@@ -78,53 +78,23 @@ def characteristics(board):
         box_height, box_width = 2, int(size / 2)
     return box_width, box_height, size
 
-def fill_board(board):
-    """
-    Fills a sudoku board of size N
-    :param board: Recursively inputs itself to replace all 0's
-    :return: Completed sudoku in the form of an array
-    """
-    if not empty_spot(board):
-        return board
-    i_row, i_col = empty_spot(board)  # Gets index of first 0
-    usable = check_usable(board, i_row, i_col)  # List of usable numbers for the position
-    random.shuffle(usable)  # Adds randomness to the board completion
-    for num in usable:
-        board[i_row][i_col] = num
-        if fill_board(board):
-            # Recursion step - will get called if that number results in a full board
-            return board
-        board[i_row][i_col] = 0  # Backtrack
 
-def number_of_solutions(board, want):
+def number_of_solutions(board):
     """
-    Ideally we only want to work with single solution boards
+    Returns the total number of solutions of the board
     :param board: Incomplete sudoku board
-    :param want: True or False, decides when to break the loop
     :return: Number of possible solutions
     """
     if not empty_spot(board):
         return 1  # If there are no empty cells, puzzle is solved
-    if want:
-        # Want to see the number of solutions
-        i_row, i_col = empty_spot(board)
-        count = 0
-        usable = check_usable(board, i_row, i_col)
-        for num in usable:
-            board[i_row][i_col] = num
-            count += number_of_solutions(board, True)
-            board[i_row][i_col] = 0  # Backtrack
-    else:
-        # This case ends as soon as the count gets above 1
-        i_row, i_col = empty_spot(board)
-        count = 0
-        usable = check_usable(board, i_row, i_col)
-        for num in usable:
-            board[i_row][i_col] = num
-            count += number_of_solutions(board, False)
-            board[i_row][i_col] = 0  # Backtrack
-            if count > 1:
-                return 2
+    # Want to see the number of solutions
+    i_row, i_col = empty_spot(board)
+    count = 0
+    usable = check_usable(board, i_row, i_col)
+    for num in usable:
+        board[i_row][i_col] = num
+        count += number_of_solutions(board)
+        board[i_row][i_col] = 0  # Backtrack
     return count
 
 def difficulty_sum(board):
@@ -139,55 +109,6 @@ def difficulty_sum(board):
             total += len(check_usable(board, i_row, i_col))
     return total
 
-def remove_numbers(board, difficulty):
-    # Easy, Medium, and Hard values are based off of test cases
-    # Need to make this code more efficient
-    size = len(board)
-    if difficulty.lower()[0] == "e":
-        difficulty = int((size ** 3) * .24)
-    elif difficulty.lower()[0] == "m":
-        difficulty = int((size ** 3) * .33)
-    elif difficulty.lower()[0] == "h":
-        difficulty = int((size ** 3) * .38)
-    while difficulty_sum(board) < difficulty:
-        row, col = random.randrange(size), random.randrange(size)
-        while board[row][col] == 0:
-            # Saves some time by preventing replacing 0s in the board
-            row, col = random.randrange(size), random.randrange(size)
-        temp, board[row][col] = board[row][col], 0
-        if number_of_solutions(board, False) > 1:
-            # Puts number back if there becomes more solutions
-            board[row][col] = temp
-    return board
-
-
-def generate_easy_board():
-    solvable_board = remove_numbers(fill_board([[0 for _ in range(9)] for _ in range(9)]), "Easy")
-    return solvable_board
-
-
-def generate_medium_board():
-    solvable_board = remove_numbers(fill_board([[0 for _ in range(9)] for _ in range(9)]), "Medium")
-    return solvable_board
-
-
-def generate_hard_board():
-    solvable_board = remove_numbers(fill_board([[0 for _ in range(9)] for _ in range(9)]), "Hard")
-    return solvable_board
-
-
-def generate_empty_board():
-    solvable_board = \
-        [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    return solvable_board
 
 # Takes user input to adjust size/difficulty of sudoku board - N needs to be a square number
 # dimension = int(input("What size sudoku would you like to try? Enter in form NxN: ").partition('x')[0])
