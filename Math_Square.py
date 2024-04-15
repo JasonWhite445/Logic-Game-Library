f4cae9c29e96f124be41783612fff52bb092557a
AdamGruhn <akgoggles@yahoo.com>
2024-04-15 11:22:02 -0500

    Window resize is functioning

--- Math_Square.py
+++ Math_Square.py
@@ -12,26 +12,33 @@ WORKS IN PROGRESS
 -Make more selection screens like Sudoku
 """
 
+# Size will be changed later to be user input
+size = 10
 background_color = (255, 255, 255)
 original_grid_element_color = (91, 114, 138)
 font_size = 20
+original_scale = 50
+screen_size = 2 * original_scale * (size + 1.25)
+if screen_size > min(pygame.display.Info().current_h, pygame.display.Info().current_w):
+    screen_size = min(pygame.display.Info().current_h, pygame.display.Info().current_w) - 100
+    original_scale = round(screen_size / (2 * (size + 1.25)), 3)
 myfont = pygame.font.SysFont('Comic Sans MS', font_size)
-my_op_font = pygame.font.SysFont('Comic Sans Ms', 30)
 pygame_icon = pygame.image.load('Smaller_Ean.png')
 pygame.display.set_icon(pygame_icon)
-# Size will be changed later to be user input
-size = 5
-# width, height = 3, 3
+
 nums = [i+1 for i in range(size**2)]
 random.shuffle(nums)
 # Grid is a random array of the numbers from 1 to size^2
 grid = [nums[size*i:size*(i+1)] for i in range(size)]
-print(grid)
+# print(grid)
 operators = ['+', '-', '*', '/']
 row_operators, col_operators = [], []
 answers = []
 
 def main():
+    global original_scale
+    global screen_size
+
     for r in range(size):
         row_signs = []
         temp = f'{grid[r][0]}'
@@ -50,7 +57,7 @@ def main():
         row_operators.append(row_signs)
         answers.append(int(eval(temp)))
         # print(f'{temp} = {int(eval(temp))}')
-        print(row_signs)
+        # print(row_signs)
 
     for r in range(size):
         col_signs = []
@@ -70,71 +77,91 @@ def main():
         col_operators.append(col_signs)
         answers.append(int(eval(temp)))
         # print(f'{temp} = {int(eval(temp))}')
-        print(col_signs)
+        # print(col_signs)
 
-    win = pygame.display.set_mode((100 * (size + 1.25), 100 * (size + 1.25)))
+    win = pygame.display.set_mode((screen_size, screen_size), pygame.RESIZABLE)
     pygame.display.set_caption(f"{size}x{size} Math Square")
     win.fill(background_color)
 
-    # Drawing empty grid
-    for i in range(0, 2 * size):
-        # Main Grid
-        pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, size*100), 2)
-        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (100*size, 50 + 50 * i), 2)
-        # Answer Cells
-        pygame.draw.line(win, (0, 0, 0),
-                         (100 * (size + 0.25), 50 + 50 * i),
-                         (100 * (size + 0.75), 50 + 50 * i), 2)
-        pygame.draw.line(win, (0, 0, 0),
-                         (50 + 50 * i, 100 * (size + 0.25)),
-                         (50 + 50 * i, 100 * (size + 0.75)), 2)
-        pygame.draw.line(win, (0, 0, 0),
-                         (100 * (size + 0.25) + 50 * (i // size), 50 + 100 * (i % size)),
-                         (100 * (size + 0.25) + 50 * (i // size), 100 + 100 * (i % size)), 2)
-        pygame.draw.line(win, (0, 0, 0),
-                         (50 + 100 * (i % size), 100 * (size + 0.25) + 50 * (i // size)),
-                         (100 + 100 * (i % size), 100 * (size + 0.25) + 50 * (i // size)), 2)
-    # Drawing black squares
-    for i in range(0, (size - 1)**2):
-        pygame.draw.rect(win, (0, 0, 0),
-                         (100 + 100 * (i // (size - 1)), 100 + 100 * (i % (size - 1)), 50, 50))
-        pass
+    def draw_grid(scale):
+        win.fill(background_color)
+        # Drawing empty grid
+        for i in range(0, 2 * size):
+            # Main Grid
+            pygame.draw.line(win, (0, 0, 0),
+                             (scale * (i + 1), scale),
+                             (scale * (i + 1), 2 * scale * size), 2)
+            pygame.draw.line(win, (0, 0, 0),
+                             (scale, scale * (i + 1)),
+                             (2 * scale * size, scale * (i + 1)), 2)
+            # Answer Cells
+            pygame.draw.line(win, (0, 0, 0),
+                             (2 * scale * (size + 0.25), scale * (i + 1)),
+                             (2 * scale * (size + 0.75), scale * (i + 1)), 2)
+            pygame.draw.line(win, (0, 0, 0),
+                             (scale * (i + 1), 2 * scale * (size + 0.25)),
+                             (scale * (i + 1), 2 * scale * (size + 0.75)), 2)
+            pygame.draw.line(win, (0, 0, 0),
+                             (2 * scale * (size + 0.25) + scale * (i // size), scale * (1 + 2 * (i % size))),
+                             (2 * scale * (size + 0.25) + scale * (i // size), 2 * scale * (1 + (i % size))), 2)
+            pygame.draw.line(win, (0, 0, 0),
+                             (scale * (1 + 2 * (i % size)), 2 * scale * (size + 0.25) + scale * (i // size)),
+                             (2 * scale * (1 + (i % size)), 2 * scale * (size + 0.25) + scale * (i // size)), 2)
+        # Drawing black squares
+        for i in range(0, (size - 1)**2):
+            pygame.draw.rect(win, (0, 0, 0),
+                             (2 * scale * ((i // (size - 1))+1) + 1,
+                              2 * scale * ((i % (size - 1))+1) + 1,
+                              scale, scale))
+            pass
 
-    # Displaying grid numbers
-    for i in range(0, size):
-        for j in range(0, size):
-            pos_1, pos_2 = 75 + 100 * j, 75 + 100 * i
-            value = myfont.render(str(grid[i][j]), True, (0, 0, 0))
-            win.blit(value, value.get_rect(center=(pos_1, pos_2)))
+        # Displaying grid numbers - Only for testing
+        for i in range(0, size):
+            for j in range(0, size):
+                pos_1, pos_2 = 2 * scale * (j+.75), 2 * scale * (i+.75)
+                value = myfont.render(str(grid[i][j]), True, (0, 0, 0))
+                win.blit(value, value.get_rect(center=(pos_1, pos_2)))
 
-    # Displaying grid operators
-    for i in range(0, size):
-        for j in range(0, size - 1):
-            x, y = 125 + 100 * j, 75 + 100 * i
-            row_op = my_op_font.render(row_operators[i][j], True, (0, 0, 0))
-            col_op = my_op_font.render(col_operators[i][j], True, (0, 0, 0))
-            win.blit(row_op, row_op.get_rect(center=(x, y)))
-            win.blit(col_op, col_op.get_rect(center=(y, x)))
-    pygame.display.update()
+        # Displaying grid operators
+        my_op_font = pygame.font.SysFont('Comic Sans Ms', int(.6 * scale))
+        for i in range(0, size):
+            for j in range(0, size - 1):
+                x, y = 2 * scale * (j+1.25), 2 * scale * (i+.75)
+                row_op = my_op_font.render(row_operators[i][j], True, (0, 0, 0))
+                col_op = my_op_font.render(col_operators[i][j], True, (0, 0, 0))
+                win.blit(row_op, row_op.get_rect(center=(x, y)))
+                win.blit(col_op, col_op.get_rect(center=(y, x)))
 
-    # Displaying answers
-    for a in range(2*size):
-        h, f = 100 * (size + .5), 75 + 100 * (a % size)
-        sol = myfont.render(str(answers[a]), True, (0, 0, 0))
-        if sol.get_width() > 48:
-            large = sol.get_width()
-            ratio = large / 48
-            new_font_size = math.floor(font_size / ratio)
-            temp_font = pygame.font.SysFont('Comic Sans MS', new_font_size)
-            sol = temp_font.render(str(answers[a]), True, (0, 0, 0))
-        if a < size:
-            win.blit(sol, sol.get_rect(center=(h, f)))
-        else:
-            win.blit(sol, sol.get_rect(center=(f, h)))
-        pass
+        # Displaying answers
+        for a in range(2*size):
+            h, f = 2 * scale * (size + .5), 2 * scale * ((a % size) + .75)
+            sol = myfont.render(str(answers[a]), True, (0, 0, 0))
+            if sol.get_width() > .9 * scale:
+                large = sol.get_width()
+                ratio = large / (.9 * scale)
+                new_font_size = math.floor(font_size / ratio)
+                temp_font = pygame.font.SysFont('Comic Sans MS', new_font_size)
+                sol = temp_font.render(str(answers[a]), True, (0, 0, 0))
+            if a < size:
+                win.blit(sol, sol.get_rect(center=(h, f)))
+            else:
+                win.blit(sol, sol.get_rect(center=(f, h)))
+            pass
+    draw_grid(original_scale)
 
     while True:
         for event in pygame.event.get():
+            if event.type == pygame.VIDEORESIZE:
+                # Calculate the new size while keeping the aspect ratio square
+                if event.w == screen_size:
+                    screen_size = event.h
+                elif event.h == screen_size:
+                    screen_size = event.w
+                else:
+                    screen_size = max(event.w, event.h)
+                win = pygame.display.set_mode((screen_size, screen_size), pygame.RESIZABLE)
+                new_scale = round(screen_size / (2 * (size + 1.25)), 3)
+                draw_grid(new_scale)
 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return
